504717caef35f0368d78675e336fe8ab35646111
yang <15136245090@163.com>
2017-08-04 17:45:53 +0800

    return

--- src/main/java/banger/action/dao/OrdersDAO.java
+++ src/main/java/banger/action/dao/OrdersDAO.java
@@ -4,10 +4,6 @@ package banger.action.dao;
  * Created by Administrator on 2017/8/3.
  */
 public interface OrdersDAO {
-	/***
-	 * yangzhixiangnnnnn    123456
-	 * @return
-	 */
 
 	int insert();
 }
