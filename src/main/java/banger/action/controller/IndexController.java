a29cda483bc8e2afe2d6f84e2d5b4173563ea9ea
ZgxDream <15239642754@163.com>
2017-08-07 10:56:48 +0800

    包结构的完善， Controller 控制层的实现

---
+++ src/main/java/banger/action/controller/IndexController.java
@@ -0,0 +1,13 @@
+package banger.action.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Zgx zhangguangxing on 2017/8/7.
+ * 该控制主要使用于对项目启动或者是用户登录后的首页
+ */
+@Controller
+@RequestMapping("")
+public class IndexController {
+}
